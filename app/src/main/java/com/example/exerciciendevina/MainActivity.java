3cba01ba776069ebadcacd2407b12917951f6dd0
AlbertFrancisco <albertfr2021@outlook.es>
2022-09-23 19:19:50 +0200

    .

--- app/src/main/java/com/example/exerciciendevina/MainActivity.java
+++ app/src/main/java/com/example/exerciciendevina/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.exerciciendevina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Random;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Random aleatori=new Random();
+
+        int contadorIntents=0;
+
+        boolean jocActiu=true;
+
+        EditText inputUsuari=findViewById(R.id.inputUsuari);
+        String numeroIntent=inputUsuari.toString();
+
+        TextView vistaContador=findViewById(R.id.contIntents);
+        vistaContador.setText(Integer.toString(contadorIntents));
+        TextView vistaPista=findViewById(R.id.pistaText);
+
+        int numeroEndevinar= aleatori.nextInt(100);
+        vistaPista.setText(Integer.toString(numeroEndevinar));
+
     }
 }
