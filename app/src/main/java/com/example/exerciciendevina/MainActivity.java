6b0cff5fc6e40333e74fa2d3b5da27ca059f51dd
Albertfrgo <92680605+Albertfrgo@users.noreply.github.com>
2022-09-26 11:18:43 +0200

    Introduccio d'un numero i missatges informatius

--- app/src/main/java/com/example/exerciciendevina/MainActivity.java
+++ app/src/main/java/com/example/exerciciendevina/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.exerciciendevina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.util.Random;
@@ -10,26 +11,47 @@ import java.util.Random;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int contadorIntents=0;
+    Random aleatori=new Random();
+    boolean jocActiu=true;
+    int numeroEndevinar= aleatori.nextInt(100);
+    String numEndStr=String.valueOf(numeroEndevinar);
+    //Per poder utilitzarlos primer instancia els EditText i TextView fora del onCreate i alla els defineixo
+    EditText inputUsuari;
+    TextView vistaContador;
+    TextView vistaPista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Random aleatori=new Random();
 
-        int contadorIntents=0;
-
-        boolean jocActiu=true;
-
-        EditText inputUsuari=findViewById(R.id.inputUsuari);
-        String numeroIntent=inputUsuari.toString();
-
-        TextView vistaContador=findViewById(R.id.contIntents);
+        inputUsuari=findViewById(R.id.inputUsuari);
+        vistaContador=findViewById(R.id.contIntents);
         vistaContador.setText(Integer.toString(contadorIntents));
-        TextView vistaPista=findViewById(R.id.pistaText);
+        vistaPista=findViewById(R.id.pistaText);
 
-        int numeroEndevinar= aleatori.nextInt(100);
+//      De momento mostra el numero que s'ha d'endevinar
         vistaPista.setText(Integer.toString(numeroEndevinar));
+    }
 
+    public void premerBoto(View view) {
+
+        try{
+            String numeroEntrat= inputUsuari.getText().toString();
+            int numeroInt=Integer.parseInt(numeroEntrat);
+            contadorIntents++;
+            vistaContador.setText(Integer.toString(contadorIntents));
+            if(numeroInt==numeroEndevinar){
+                vistaPista.setText("\nHAS ENCERTAT EL NUMERO!!!");
+            }else if(numeroInt<numeroEndevinar){
+                vistaPista.setText("\nEl numero "+numeroEntrat+" es inferior");
+            }else if(numeroInt>numeroEndevinar) {
+                vistaPista.setText("\nEl numero "+numeroEntrat + " es superior");
+            }
+
+        }catch (NumberFormatException e) {
+            vistaPista.setText("\nNo has entrat un numero");
+        }
     }
 }
