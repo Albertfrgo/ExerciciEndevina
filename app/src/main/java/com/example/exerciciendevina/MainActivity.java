096380bdc58dd6982370396b12fb84ac95a4f0ca
AlbertFrancisco <albertfr2021@outlook.es>
2022-09-26 17:13:28 +0200

    Introduccio d'un numero i missatges informatius

--- app/src/main/java/com/example/exerciciendevina/MainActivity.java
+++ app/src/main/java/com/example/exerciciendevina/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.exerciciendevina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.Random;
 
 import java.util.Random;
@@ -30,28 +34,35 @@ public class MainActivity extends AppCompatActivity {
         vistaContador=findViewById(R.id.contIntents);
         vistaContador.setText(Integer.toString(contadorIntents));
         vistaPista=findViewById(R.id.pistaText);
+        vistaPista.setMovementMethod(new ScrollingMovementMethod());
 
 //      De momento mostra el numero que s'ha d'endevinar
         vistaPista.setText(Integer.toString(numeroEndevinar));
     }
 
     public void premerBoto(View view) {
-
-        try{
-            String numeroEntrat= inputUsuari.getText().toString();
-            int numeroInt=Integer.parseInt(numeroEntrat);
+        String textToast="Boto apretat";
+        Toast.makeText(getApplicationContext(),textToast,Toast.LENGTH_SHORT).show();
+        Editable numeroEntrat= inputUsuari.getText();
+        if(inputUsuari.getText().toString().equals("")==false){
+            int numeroInt=Integer.parseInt(numeroEntrat.toString());
             contadorIntents++;
             vistaContador.setText(Integer.toString(contadorIntents));
             if(numeroInt==numeroEndevinar){
                 vistaPista.setText("\nHAS ENCERTAT EL NUMERO!!!");
             }else if(numeroInt<numeroEndevinar){
-                vistaPista.setText("\nEl numero "+numeroEntrat+" es inferior");
+                //vistaPista.setText("\nEl numero "+numeroEntrat+" es inferior");
+                vistaPista.append("\nEl numero "+numeroEntrat+" es inferior");
+                inputUsuari.setText("");
             }else if(numeroInt>numeroEndevinar) {
-                vistaPista.setText("\nEl numero "+numeroEntrat + " es superior");
-            }
+                //vistaPista.setText("\nEl numero "+numeroEntrat + " es superior");
+                vistaPista.append("\nEl numero "+numeroEntrat + " es superior");
+                inputUsuari.setText("");
+        }
 
-        }catch (NumberFormatException e) {
-            vistaPista.setText("\nNo has entrat un numero");
+        }else{
+        vistaPista.append("\nNo has entrat un numero");
+        inputUsuari.setText("");
         }
     }
 }
